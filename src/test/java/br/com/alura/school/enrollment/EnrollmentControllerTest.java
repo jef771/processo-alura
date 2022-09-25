54f8dcc68a32f7eef0302e994352b2fb742d6726
jef771 <jefh.neves@gmail.com>
2022-09-25 17:42:31 -0300

    Bonus - Adiciona Teste para Salvar novo Enrollment

---
+++ src/test/java/br/com/alura/school/enrollment/EnrollmentControllerTest.java
@@ -0,0 +1,60 @@
+package br.com.alura.school.enrollment;
+
+import br.com.alura.school.course.Course;
+import br.com.alura.school.course.CourseRepository;
+import br.com.alura.school.lecture.LectureRepository;
+import br.com.alura.school.user.User;
+import br.com.alura.school.user.UserRepository;
+import br.com.alura.school.user.UserRole;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class EnrollmentControllerTest {
+
+    private final ObjectMapper jsonMapper = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private EnrollmentRepository enrollmentRepository;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void init() {
+        courseRepository.save(new Course("java-1", "Java OO", "Java and Object Orientation: Encapsulation, Inheritance and Polymorphism."));
+        User newUser = userRepository.save(new User("ana", "ana@email.com"));
+        newUser.setRole(UserRole.INSTRUCTOR);
+        userRepository.save(newUser);
+    }
+
+    @Test
+    void should_enroll() throws Exception {
+        NewEnrollmentRequest newEnrollmentRequest = new NewEnrollmentRequest("ana");
+
+        mockMvc.perform(post("/courses/java-1/enroll")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonMapper.writeValueAsString(newEnrollmentRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", ""));
+    }
+}
